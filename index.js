509961d0e73c6447a4457448e704de87d3ea687f
JosephEllie <josephelliecooper@gmail.com>
2023-10-12 09:35:29 +0000

    added dependencies

--- index.js
+++ index.js
@@ -19,24 +19,26 @@ app.post('/ussd', (req, res) => {
     if (text == '') {
         // This is the first request. Note how we start the response with CON
         response = `CON What would you like to check
-        1. My account
-        2. My phone number`;
+        1. Check Balances
+        2. Send Funds
+        3. Wallet Address`
     } else if (text == '1') {
         // Business logic for first level response
-        response = `CON Choose account information you want to view
-        1. Account number`;
+        response = `END You will be notified shortly`;
+
     } else if (text == '2') {
         // Business logic for first level response
         // This is a terminal request. Note how we start the response with END
-        response = `END Your phone number is ${phoneNumber}`;
-    } else if (text == '1*1') {
+        response = `CON Select Token 
+            ${token}
+        `;
+    } else if (text == '3') {
         // This is a second level response where the user selected 1 in the first instance
-        const accountNumber = 'ACC100101';
         // This is a terminal request. Note how we start the response with END
-        response = `END Your account number is ${accountNumber}`;
+        response = `END You will be notified shortly`;
     }
-
     // Send the response back to the API
     res.set('Content-Type: text/plain');
     res.send(response);
 });
+
